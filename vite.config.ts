257bbbd203d033d98866baa53db428a7cf57a1b1
Javivi <user@MacBook-Pro-de-User.local>
2024-12-05 16:05:01 +0100

    add vite configuration to connect with backend

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": "/src", // This maps the @ alias to the src directory
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
